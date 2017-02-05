c84572e34716c63ccc38c8315b1e056c703ce1ad
Kaidaki <kaidaki33@gmail.com>
2017-02-05 15:46:05 +0900

    fix source file test
    test

--- Assets/test.cs
+++ Assets/test.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class test : MonoBehaviour {
+
+//필수 연관 컴포넌트 자동 포함
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class test : MonoBehaviour
+{
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 }
